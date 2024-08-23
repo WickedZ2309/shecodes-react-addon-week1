ee2bb9c7eeea268a437f38b7ba9e6c2600a492ec
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-23 00:37:44 -0400

    Added the search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,38 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/WickedZ2309"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Andrea G.
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/WickedZ2309/shecodes-react-addon-week1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://shecodes-react-addon-week1-ag.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          .
+        </small>
+      </footer>
     </div>
   );
 }
