f7d0188fbc5f52dd1766f1a4fc6d7c10c696fcac
WickedZ2309 <168111978+WickedZ2309@users.noreply.github.com>
2024-08-23 00:54:44 -0400

    Added api documentation url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,7 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
+    //documentation : https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "9847fe0311559eao53bt6fd43e08ac31";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
 
